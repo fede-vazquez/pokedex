2d5cb746d574302d7c5c9d84f1cb4580d9f71ef3
fede-vazquez <fedenv2000@gmail.com>
2025-06-28 18:53:34 -0300

    Agrega estilos y estructura para las cards de pokemones, con colores de fondo distintos para cada tipo.

--- src/components/generatePokemonCard.js
+++ src/components/generatePokemonCard.js
@@ -1,35 +1,43 @@
+import "../styles/pokemon-card.css";
+
 /**
  * Función que genera una card de un pokémon dependiendo de la información dada y la agrega al elemento HTML dado como padre.
  * @param {Object} pokemon - Objeto que contiene la información del pokémon.
  * @param {string} pokemon.name - Nombre del pokémon.
  * @param {number} pokemon.pokedexNumber - Numero de la pokedex del pokémon.
- * @param {number} pokemon.order - Orden del pokémon.
  * @param {Array<string>} pokemon.typeNames - Nombres de los tipos del pokémon.
  * @param {string} pokemon.sprite - URL de la imagen del pokémon.
  * @param {HTMLElement} parent - Elemento padre donde se añadirá la card.
  */
 
 export const generatePokemonCard = (
-    { name, pokedexNumber, order, typeNames, sprite },
+    { name, pokedexNumber, typeNames, sprite },
     parent
 ) => {
-    console.log("Generating card for:", name, order, typeNames, sprite);
     const newCard = document.createElement("li");
     newCard.classList.add("pokemon-card");
+    newCard.classList.add(`type-${typeNames[0]}`);
 
     newCard.innerHTML = `
-    <div class="card-header">
-        <h2 class="card-name">${name}</h2>
-        <span class="card-pokedex-number">${pokedexNumber}</span>
-        <div class="card-img">
-            <img src="${sprite}" alt="imágen de ${name}">
+    <a href="">
+        <div class="card-header">
+            <h2>${name}</h2>
+            <span>N° ${pokedexNumber}</span>
         </div>
-    </div>
-    <div class="card-info">
-        <ul class="card-types">
-            ${typesItems(typeNames)}
-        </ul>
-    </div>
+        <div class="card-info">
+        <div class="card-types-container">
+            <h5>Tipos</h5>
+            <ul class="card-types">
+                ${typesItems(typeNames)}
+            </ul>
+        </div>
+
+            <div class="card-img">
+                <img src="${sprite}" alt="imágen de ${name}">
+            </div>
+
+        </div>
+    <a/>
     `;
 
     parent.appendChild(newCard);
