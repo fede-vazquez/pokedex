f2d5be81d1259bda028ab7fea083f03bdf07ae9c
fede-vazquez <fedenv2000@gmail.com>
2025-06-29 20:48:53 -0300

    Modifica implementación dentro de generateGenerationList. Para evitar el re-renderizado innecesario del elemento. Y le da estílos

--- src/components/generateGenerationBtn.js
+++ src/components/generateGenerationBtn.js
@@ -1,21 +1,19 @@
 /**
  * Función que devuelve elemento li que va a servir para hacer búsquedas por generación.
  * @param {Number} genID - ID de la generación.
- * @param {Number} actualGenID - ID de la generación actual.
  * @param {Function} handleChangeGen - Función encargada de cambiar la generación mostrada.
  * @returns {HTMLElement} - Item botón para cambiar la generación actual.
  */
 
-export const generateGenerationBtn = (genID, actualGenID, handleChangeGen) => {
+export const generateGenerationBtn = (genID, handleChangeGen) => {
     const listItem = document.createElement("li");
     listItem.classList.add("generation-item");
-    if (genID === actualGenID) listItem.classList.add("active-gen");
 
     const genBtn = document.createElement("button");
     genBtn.classList.add("generation-btn");
     genBtn.innerText = `Generación: ${genID}`;
 
-    if (genID !== actualGenID) genBtn.onclick = () => handleChangeGen(genID);
+    genBtn.onclick = e => handleChangeGen(e.target.parentNode, genID);
 
     listItem.appendChild(genBtn);
 
--- src/components/generateGenerationList.js
+++ src/components/generateGenerationList.js
@@ -1,27 +1,35 @@
 import { handleChangeGen } from "../main";
 import { generateGenerationBtn } from "./generateGenerationBtn";
+import "../styles/generation-list.css";
 
 /**
  * Función que genera elemento ul con una lista de generaciones pokémon para su selección.
  * @param {Array<number>} genIDs - IDs de generaciones pokémon.
- * @param {Number} actualGenID - Generación seleccionada
  * @returns {HTMLElement} - Elemento ul que contiene toda la lista de generaciones para seleccionar.
  */
 
-export const generateGenerationList = (genIDs, actualGenID) => {
+export const generateGenerationList = genIDs => {
     const list = document.createElement("ul");
     list.classList.add("generation-list");
+
+    const handleClick = (element, genNumber) => {
+        const selectedItem = document.querySelector(".active-gen");
+        selectedItem?.classList.remove("active-gen");
+
+        element.classList.add("active-gen");
+
+        handleChangeGen(genNumber);
+    };
 
     for (let i = 0; i < genIDs.length; i++) {
         const genID = genIDs[i];
 
-        const listItem = generateGenerationBtn(
-            genID,
-            actualGenID,
-            handleChangeGen
-        );
+        const listItem = generateGenerationBtn(genID, handleClick);
         list.appendChild(listItem);
     }
+
+    // Ejecutamos la primera vez que se genera la lista.
+    handleClick(list.firstChild, 1);
 
     return list;
 };
